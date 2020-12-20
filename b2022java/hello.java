f0b72905a15a2b9917a112d7fc77f94de1a0f784
mlapinm <mlapin@rambler.ru>
2020-12-20 09:25:41 +0300

    2

---
+++ b2022java/hello.java
@@ -0,0 +1,12 @@
+import java.util.logging.Logger;
+
+public class Hello{
+  /**
+   *
+   */
+  
+  public static void main(String[] args) {
+    System.out.println("AAAAA1");
+    logger("dddd");
+  }
+}
